86c0f3fc9ec11b600a8d0802fa2bc7d00e8f9644
sepro <4618135+seproDev@users.noreply.github.com>
2022-06-18 01:54:24 +0200

    Improve macro/module display

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,35 @@
 const markdownIt = require("markdown-it");
 const markdownItRenderer = new markdownIt();
+const compareVersions = require('compare-versions');
 
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy('src/static');
-	eleventyConfig.addFilter('markdownify', (str) => {
-		return markdownItRenderer.renderInline(str);
-	});
+    eleventyConfig.addPassthroughCopy('src/static');
+    eleventyConfig.addFilter('markdownify', (str) => {
+        return markdownItRenderer.renderInline(str);
+    });
+    eleventyConfig.addFilter("changelogSort", function (changelog) {
+        if (changelog) {
+            var items = Object.entries(changelog);
 
-	return {
-		dir: {
-			input: "src/",
-			output: "_site",
-			includes: "_includes",
-			layouts: "_layouts"
-		},
-		passthroughFileCopy: true,
-		templateFormats: ["html", "md", "njk"],
-		htmlTemplateEngine: "njk",
-	};
+            items.sort((first, second) => {
+                return compareVersions(second[0], first[0]);
+            });
+
+            return Object.fromEntries(items);
+        } else {
+            return {};
+        }
+    });
+
+    return {
+        dir: {
+            input: "src/",
+            output: "_site",
+            includes: "_includes",
+            layouts: "_layouts"
+        },
+        passthroughFileCopy: true,
+        templateFormats: ["html", "md", "njk"],
+        htmlTemplateEngine: "njk",
+    };
 };
